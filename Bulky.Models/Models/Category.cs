dc8a5cb8f4cc2de0ed43e5f52b5ade9c6b5558ce
Shubhrajeet Ghosh <shubhrajeetghosh3@gmail.com>
2023-06-11 00:30:49 +0530

    Add project files.

---
+++ Bulky.Models/Models/Category.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace BulkyBook.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(30)]
+        [DisplayName("Category Name")]
+        public string Name { get; set; }
+        [DisplayName("Display Order")]
+        [Range(0, 100,ErrorMessage ="Range should be between 0-100")]
+        public int DisplayOrder { get; set; }
+    }
+}
